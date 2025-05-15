12ab75e18a4293d6199fa753d5913546c5b54dee
Vergil-is-here <hiddenboy7@gmail.com>
2025-05-15 18:13:21 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+cd path/to/product-scraper-api   # برو داخل همون پوشه‌ای که پروژه هست
+git init
+git remote add origin https://github.com/USERNAME/product-scraper.git
+git add .
+git commit -m "first commit"
+git push -u origin master
